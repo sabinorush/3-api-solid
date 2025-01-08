da3a68129f22605bee3ae8e3603aa6c8c030d20d
Gustavo Sabino <gu.sabino@hotmail.com>
2025-01-08 13:11:28 -0300

    Caso de uso e Design Patterna - Hash da senha e validação

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes';
+
 
 export const app = fastify();
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Gustavo',
-    email: 'gu.sabino@hotmail.com',
-  },
-})
+app.register(appRoutes)
