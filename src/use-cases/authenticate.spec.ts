eece029409d25b52468fcdca21094c1d184d800e
Gustavo Sabino <gu.sabino@hotmail.com>
2025-01-09 13:53:08 -0300

    Implementando casos de uso - Refatorando instância nos testes

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,14 +1,19 @@
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository"
 import { hash } from "bcryptjs"
-import { expect, describe, it } from "vitest"
+import { expect, describe, it, beforeEach } from "vitest"
 import { AuthenticateUseCase } from "./authenticate"
 import { InvalidCredentialsError } from "./errors/invalid-crendentials-error"
 
-describe('Authenticate Use Case', () => {
-  it('should be able to authenticate', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository) //sut => system under test
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
 
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+  })
+
+  it('should be able to authenticate', async () => {
     await usersRepository.create({
       name: 'Fulano de tal',
       email: 'fuladetal@exemplo.com',
@@ -24,21 +29,16 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong email', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository) //sut => system under test
-
     await expect(() =>
       sut.execute({
         email: 'fuladetal@exemplo.com',
         password: '123456'
       }),
     ).rejects.toBeInstanceOf(InvalidCredentialsError)
+    
   })
 
   it('should not be able to authenticate with wrong password', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(usersRepository) //sut => system under test
-
     await usersRepository.create({
       name: 'Fulano de tal',
       email: 'fuladetal@exemplo.com',
