02ac9215cf91be7f137e764e4b7c81e9caf661a8
Gustavo Sabino <gu.sabino@hotmail.com>
2025-01-09 22:50:10 -0300

    implementando casos de uso - Validando horário do check-in

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,34 +1,45 @@
-   import { CheckInsRepository } from "@/repositories/check-ins-repository";
-   import { CheckIn } from "@prisma/client";
-   import { ResourceNotFoundError } from "./errors/resource-not-found";
-   
-   interface ValidateCheckInUseCaseRequest {
-    checkInID: string
-   }
-   
-   
-   interface ValidateCheckInUseCaseResponse {
-     checkIn: CheckIn
-   }
-   
-   export class ValidateCheckInUseCase {
-     constructor(private checkInsRepository: CheckInsRepository) { }
-   
-     async execute({
-      checkInID
-     }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
-       const checkIn = await this.checkInsRepository.findById(checkInID)
-   
-       if (!checkIn) {
-         throw new ResourceNotFoundError()
-       }
-   
-        checkIn.validated_at = new Date()
+import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { CheckIn } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found";
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
-        await this.checkInsRepository.save(checkIn)
-   
-       return {
-         checkIn,
-       }
-     }
-   }
+interface ValidateCheckInUseCaseRequest {
+  checkInID: string
+}
+
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) { }
+
+  async execute({
+    checkInID
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInID)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
